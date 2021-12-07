66a444823f46a07a2aa69c14a3d74c3d963a8fc4
Sarathcani999 <mailsforsarath98@gmail.com>
2021-12-07 21:54:25 +0530

    code added

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// apis
+app.get('/' , (req,res) => {
+	var outputJSON = {
+		"message" : "sucess"
+	};
+	res.json(outputJSON);
+});
+
+const PORT = process.env.PORT | 3000;
+app.listen(PORT , (err) => {
+	console.log("Listening at PORT :",PORT);
+});
