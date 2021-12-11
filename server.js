4aa94b826f5a448b1698ed16c1268229f533c12d
Sarathcani999 <mailsforsarath98@gmail.com>
2021-12-11 12:36:46 +0530

    test api added

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/' , (req,res) => {
 	res.json(outputJSON);
 });
 
+app.get('/test' , (req,res) => {
+	var outputJSON = {
+		"message" : "test success"
+	};
+	res.json(outputJSON);
+});
+
 const PORT = process.env.PORT | 3000;
 app.listen(PORT , (err) => {
 	console.log("Listening at PORT :",PORT);
